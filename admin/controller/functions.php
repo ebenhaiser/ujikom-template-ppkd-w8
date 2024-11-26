a098d2c185d897cc0e1a35c670574d338b895626
nasywasyifa <ebenhaiser@gmail.com>
2024-11-26 08:46:06 +0700

    edit

--- admin/controller/functions.php
+++ admin/controller/functions.php
@@ -4,9 +4,6 @@ require_once 'connection.php';
 function loginController($email, $password)
 {
     global $connection;
-    $email    = $_POST['email']; //untuk mengambil nilai dari input
-    $password = $_POST['password'];
-
     $queryLogin = mysqli_query($connection, "SELECT * FROM user WHERE email='$email' AND deleted_at=0
     ");
     // mysqli_num_row() : untuk melihat total data di dalam table
@@ -23,5 +20,14 @@ function loginController($email, $password)
     } else {
 
         return false;
+    }
+}
+
+function loginValidation()
+{
+    if (!isset($_SESSION['id'])) {
+        return false;
+    } else {
+        return true;
     }
 }
